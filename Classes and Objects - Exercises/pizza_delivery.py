e3017565ad854a3ed6b730115d7711f767faa618
Svetoslav Baykov <svetoslavbaykov55@gmail.com>
2023-03-08 08:25:30 +0200

    Code for exercise pizza_delivery updated

--- Classes and Objects - Exercises/pizza_delivery.py
+++ Classes and Objects - Exercises/pizza_delivery.py
@@ -38,14 +38,3 @@ class PizzaDelivery:
             return f"You've ordered pizza {self.name} prepared with {', '.join(ingrediants_list)} and the price will be {self.price}lv."
 
         return f"Pizza {self.name} already prepared, and we can't make any changes!"
-    
-
-margarita = PizzaDelivery('Margarita', 11, {'cheese': 2, 'tomatoes': 1})
-margarita.add_extra('mozzarella', 1, 0.5)
-margarita.add_extra('cheese', 1, 1)
-margarita.remove_ingredient('cheese', 1, 1)
-print(margarita.remove_ingredient('bacon', 1, 2.5))
-print(margarita.remove_ingredient('tomatoes', 2, 0.5))
-margarita.remove_ingredient('cheese', 2, 1)
-print(margarita.make_order())
-print(margarita.add_extra('cheese', 1, 1))
